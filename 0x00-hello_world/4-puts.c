0b78f46a63ac20db7b3e93a5117a08f08213e5b5
Kingsley <louis.officialz@gmail.com>
2023-09-08 00:58:07 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main - This is c programming language
+ *  Return: Always 0 for Success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
