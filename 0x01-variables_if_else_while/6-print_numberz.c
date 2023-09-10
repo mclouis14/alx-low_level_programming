0da84d4f03b2643b7c66cdbaabd8cbed09fe4142
Kingsley <louis.officialz@gmail.com>
2023-09-10 11:17:02 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This is the function for the task
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar(a + '0');
+	putchar('\n');
+	return (0);
+}
