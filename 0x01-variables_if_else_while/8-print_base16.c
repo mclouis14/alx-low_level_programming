ece90cbcce606bdc33a1cc01275eafc59204d85f
Kingsley <louis.officialz@gmail.com>
2023-09-10 14:03:26 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - This is the function for the task
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 16; a++)
+	{
+	if (a < 10)
+	putchar('0' + a);
+	}
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
