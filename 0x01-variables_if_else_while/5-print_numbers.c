4e6ed46b4277b02688d946dd8f3d6ba57e1ab2f0
Kingsley <louis.officialz@gmail.com>
2023-09-10 10:16:53 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This is the function of the task
+ * Return: Always 0
+ */
+int main(void)
+{
+	int 1;
+
+	for (1 = 0; 1 <= 9; 1++)
+	{
+	printf(1)
+	}
+	return (0);
+}
