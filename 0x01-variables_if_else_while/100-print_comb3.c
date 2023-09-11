cadf0c73bcd40a62ff20dce484e8eeaa60553756
Kingsley <louis.officialz@gmail.com>
2023-09-10 17:40:40 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - This is the function for the task
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i; j <= 9; j++)
+		{
+			if (i != j)
+			{
+				putchar(i + '0');
+				putchar(',');
+				putchar(' ');
+				putchar(j + '0');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
+
