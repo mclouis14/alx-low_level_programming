1817018c9709b10577def138eeeb4a5594dcce8f
Kingsley <louis.officialz@gmail.com>
2023-09-14 19:23:13 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - the function to ascertain for a digit (0 through).
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
