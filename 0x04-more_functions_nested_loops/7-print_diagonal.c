3d202f96df7e9a96cbe2136db39bda8342d144ce
Kingsley <louis.officialz@gmail.com>
2023-09-15 01:02:14 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - the function prints diagonal using the '\' character
+ * @n: number of times the '\' should be printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int a, b;
+
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b <= a; b++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
