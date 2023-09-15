971ded9238f81a369ea3811e0274439bbaf99c9c
Kingsley <louis.officialz@gmail.com>
2023-09-14 20:21:19 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - the function checks to ascertain for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i + '0');
+	}
+		_putchar('\n');
+}
