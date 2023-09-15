e24189c04700848f42aa00f86be7ba37702c5cec
Kingsley <louis.officialz@gmail.com>
2023-09-15 01:10:59 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,9 +14,9 @@ void print_diagonal(int n)
 	{
 		int a, b;
 
-		for (a = 0; a < n; a++)
+		for (a = 0 ; a < n ; a++)
 		{
-			for (b = 0; b <= a; b++)
+			for (b = 0 ; b <= a ; b++)
 				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
