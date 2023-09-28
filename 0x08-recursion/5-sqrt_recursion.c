a76412b6fb3cf43e9fcddf75f222571bd9beeffc
Kingsley <louis.officialz@gmail.com>
2023-09-28 16:19:09 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,24 +20,24 @@ int _sqrt_recursion(int n)
 		int start = 1;
 		int end = n;
 
-		while (start <= end)
-		{
-			int mid = start + (end - start) / 2;
-			int square = mid * mid;
+	while (start <= end)
+	{
+		int mid = start + (end - start) / 2;
+		int square = mid * mid;
 
-			if (square == n)
-			{
-				return (mid);
-			}
-			else if (square < n)
-			{
-				start = mid + 1;
-			}
-			else
-			{
-				end = mid - 1;
-			}
+		if (square == n)
+		{
+			return (mid);
 		}
-		return (-1);
+		else if (square < n)
+		{
+			start = mid + 1;
+		}
+		else
+		{
+			end = mid - 1;
+		}
+	}
+	return (-1);
 	}
 }
