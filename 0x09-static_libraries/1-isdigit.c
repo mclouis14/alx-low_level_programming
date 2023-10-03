3faf74efa1227dd1d21972d8b5e0785d6b177d5c
Kingsley <louis.officialz@gmail.com>
2023-10-03 10:21:38 -0700

    libmy.a

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - the function to check for a digit (0 through 9)
+ * @c: inter to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
