78da0dd4e0c27af46dfaa216f79c38e20afbeddb
Kingsley <louis.officialz@gmail.com>
2023-10-19 20:14:32 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,15 @@ size_t print_list(const list_t *h)
 {
 	size_t node_count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%lu] %s\n", node_count, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		node_count++;
 	}
+
 	return (node_count);
 }
