4f273ad0997281f22f2b30b881f4882ff0534650
Kingsley <louis.officialz@gmail.com>
2023-10-20 16:22:13 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@ void startup_message(void) __attribute__ ((constructor));
  */
 void startup_message(void)
 {
-	printf("You're beat! and yet, you must allow, \n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
