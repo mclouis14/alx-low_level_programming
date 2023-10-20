05524190766b604e9268fcf40364b7593a930968
Kingsley <louis.officialz@gmail.com>
2023-10-20 16:11:10 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void startup_message(void) __attribute__((constructor));
+
+/**
+ * startup_message - The function will be executed before the main.
+ */
+void startup_message(void)
+{
+	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
+}
