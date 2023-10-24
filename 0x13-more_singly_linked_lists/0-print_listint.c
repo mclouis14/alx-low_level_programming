ea0d2283f6cc59a15e335614104da33c1f5a57cd
Kingsley <louis.officialz@gmail.com>
2023-10-24 10:06:40 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,14 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t nodecount = 0;
+	const listint_t *current = h;
 
-	while (h)
+	while (current != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", current->n);
 		nodecount++;
+		current = current->next;
 	}
+
 	return (nodecount);
 }
