0f02a83ddc8b6cfe227fa0c2a7fe89835f8a53bc
Kingsley <louis.officialz@gmail.com>
2023-12-11 03:24:27 -0800

    main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef DYNAMIC_LIB
-#define DYNAMIC_LIB
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdlib.h>
 
@@ -24,4 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* DYNAMIC_LIB */
+#endif /* MAIN_H */
