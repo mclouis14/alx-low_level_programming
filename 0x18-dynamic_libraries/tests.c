2a3a9a24293d77caccf60c90062865694c9b2491
Kingsley <louis.officialz@gmail.com>
2023-12-11 16:06:50 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/tests.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+/**
+ * add - Adds two integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: Result of addition.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - Subtracts two integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: Result of subtraction.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - Multiply two integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: Result of multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - Divides two integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: Result of division.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - Mods two integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: Result of modulus.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
